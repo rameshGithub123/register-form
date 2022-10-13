5384767d46639829d4725aa36d25933e83682b91
rameshGithub123 <113126880+rameshGithub123@users.noreply.github.com>
2022-10-13 10:00:31 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import "./styles.css";
+import RegisterForm from "./RegisterForm";
+
+export default function App() {
+  return (
+    <div className="App">
+      <h1>Hello CodeSandbox</h1>
+      <RegisterForm />
+    </div>
+  );
+}
